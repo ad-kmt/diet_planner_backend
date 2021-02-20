0b825c63eb369401c62459ab07675cbfa65ab50a
Hitesh Kumawat <kumawat.hitesh743@gmail.com>
2021-02-21 01:14:25 +0530

    quiz conclusion post

--- routes/api/quiz.js
+++ routes/api/quiz.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { validationResult } = require('express-validator');
+const brain = require('../../ml/brain');
+
 const Quiz = require('../../models/Quiz');
 
 // @route    GET /api/quiz
@@ -15,3 +18,23 @@ router.get('/', async (req, res) => {
   }
 });
 module.exports = router;
+
+// @route    POST api/quiz
+// @desc     post quiz data
+// @access   Private
+router.post('/', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const input=req.body.map(question => question.options.map(option=> option.selected));
+      brain(input);
+      return res.status(200).send("Success");
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error');
+    }
+  }
+);
